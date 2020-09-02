7da50500b3a07db9acf1675446a127ea1b721c40
nicviswa <70627869+nicviswa@users.noreply.github.com>
2020-09-02 13:02:24 +0530

    Add files via upload

---
+++ include/config893.php
@@ -0,0 +1,31 @@
+<?php
+	$db_handle = pg_connect("host=localhost dbname=nic1 user=postgres password=password");
+	if ($db_handle) 
+	{
+		echo 'Connection attempt succeeded.';
+	} 
+	else 
+	{
+		echo 'Connection attempt failed.';
+	}
+
+	//echo "<h3>Connection Information</h3>";
+	//echo "DATABASE NAME:" . pg_dbname($db_handle) . "<br>";
+	//echo "HOSTNAME: " . pg_host($db_handle) . "<br>";
+	//echo "PORT: " . pg_port($db_handle) . "<br>";
+?>
+
+<?php
+/*
+    $db = pg_connect("host=localhost port=5432 dbname=db_test user=postgres password=123abc");//line 5
+    if($db){
+        echo "Connected\n";
+    } else {
+        echo "Error NotConnected\n";
+    }
+	
+	CREATE DATABASE cppp893 WITH OWNER = cppp ENCODING = 'UTF8' CONNECTION LIMIT = -1;
+*/
+
+
+?>
